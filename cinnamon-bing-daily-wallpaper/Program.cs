a56a9a11405fd1b0b61ccba78228453f07e5c171
Fatlum Latifi <fatlumlatif@gmail.com>
2025-03-13 19:26:16 +0100

    comments

--- cinnamon-bing-daily-wallpaper/Program.cs
+++ cinnamon-bing-daily-wallpaper/Program.cs
@@ -1,16 +1,21 @@
 ﻿using BingWallpapers;
 using Linux.Cinnamon;
 
+// Checks and creates directory to hold image
 const string directoryPath = "~/.local/share/bing-daily-wallpaper";
 DirectoryInfo di;
-if (Directory.Exists(directoryPath) is false) { di = Directory.CreateDirectory(directoryPath); }
+if (Directory.Exists(directoryPath) is false) 
+{ di = Directory.CreateDirectory(directoryPath); }
 else { di = new(directoryPath); }
+
 
 using (WallpaperDownloader bingDownloader = new(di))
 {
+    // FileInfo of the downloaded image
     FileInfo? file = await bingDownloader.DownloadDailyAsync();
     if (file is not null) 
     {
+        // Sets the wallpaper
         WallPaper.SetWallpaper(file.FullName);
     }
 }
